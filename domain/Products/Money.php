78d2ab4901d4938777827d8c6f19c9a29b9edbf9
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-01-26 20:13:56 +0100

    Add Money value object
    Same here, the scalar float is saved and checked to be positive.
    
    Money values could be zero or negative, but we have no use for that in
    our domain so we do not allow it here.

---
+++ domain/Products/Money.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Domain\Products;
+
+use RuntimeException;
+
+final class Money
+{
+    /**
+     * @var float
+     */
+    private $value;
+
+    public function __construct(float $value)
+    {
+        if ($value <= 0) {
+            return new RuntimeException('Money value must be positive');
+        }
+
+        $this->value = $value;
+    }
+
+    public static function fromValue(float $value): self
+    {
+        return new self($value);
+    }
+
+    public function getValue(): float
+    {
+        return $this->value;
+    }
+}
