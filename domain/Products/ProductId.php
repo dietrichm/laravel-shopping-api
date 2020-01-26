d820192aa907b01482410218d8d14391630f0394
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-01-26 20:07:56 +0100

    Create ProductId value object
    This will be the product ID we will use in the domain layer. It
    encapsulates a Uuid instance and can generate a new UUID 4.

---
+++ domain/Products/ProductId.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Domain\Products;
+
+use Ramsey\Uuid\Uuid;
+
+final class ProductId
+{
+    /**
+     * @var Uuid
+     */
+    private $uuid;
+
+    public function __construct(Uuid $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    public static function generate(): self
+    {
+        return new self(Uuid::uuid4());
+    }
+
+    public function toString(): string
+    {
+        return $this->uuid->toString();
+    }
+}
