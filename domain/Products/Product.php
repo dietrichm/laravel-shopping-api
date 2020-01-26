597bd3b1418e69fc974f3873c24b78f839f9aa30
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-01-26 20:10:53 +0100

    Allow ProductId to be set on Product instance
    We only save the scalar value in the domain entity (which is the
    Eloquent model) as we still want to leverage the ORM to load and save
    instances from and to the database.

--- domain/Products/Product.php
+++ domain/Products/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 final class Product extends Model
 {
-    //
+    public function setId(ProductId $productId): void
+    {
+        $this->uuid = $productId->toString();
+    }
 }
