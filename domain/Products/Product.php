84d6a2e54212cbe3b873926a45837b05ccb2364c
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-01-28 23:48:51 +0100

    Add getters for Product's properties
    We recreate the value objects when getting the properties using their
    respective getters. This way, as long as only getters and setters are
    used, the properties are always proper value objects.

--- domain/Products/Product.php
+++ domain/Products/Product.php
@@ -6,14 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 final class Product extends Model
 {
+    public function getId(): ProductId
+    {
+        return ProductId::fromString($this->uuid);
+    }
+
     public function setId(ProductId $productId): void
     {
         $this->uuid = $productId->toString();
     }
 
+    public function getName(): ProductName
+    {
+        return ProductName::fromString($this->name);
+    }
+
     public function setName(ProductName $productName): void
     {
         $this->name = $productName->toString();
+    }
+
+    public function getPrice(): Money
+    {
+        return Money::fromValue($this->price);
     }
 
     public function setPrice(Money $price): void
