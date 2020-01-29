0d14f5512ced236886535855fb681a32f31d6d5b
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-01-30 00:32:30 +0100

    Assert Product is correctly transformed into array
    We want an array having the ID, name and price which can be serialized
    into a JSON object.

---
+++ tests/Unit/Http/Resources/Products/ProductResourceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Http\Resources\Products;
+
+use App\Http\Resources\Products\ProductResource;
+use Domain\Products\Product;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+final class ProductResourceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itTransformsProductIntoArray(): void
+    {
+        $product = factory(Product::class)->make();
+
+        $resource = new ProductResource($product);
+
+        $this->assertEquals(
+            [
+                'id' => $product->getId()->toString(),
+                'name' => $product->getName()->toString(),
+                'price' => $product->getPrice()->getValue(),
+            ],
+            $resource->toArray(new Request())
+        );
+    }
+}
