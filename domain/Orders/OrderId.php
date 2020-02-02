06e1a1d6f1d349088ce6c2a1ed6c2967cc300f12
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-02 10:03:50 +0100

    Add OrderId value object to Orders domain
    Just as with a ProductId, we are using a UUID internally.

---
+++ domain/Orders/OrderId.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Domain\Orders;
+
+use Ramsey\Uuid\Uuid;
+
+final class OrderId
+{
+    /**
+     * @var Uuid
+     */
+    private $uuid;
+
+    public function __construct(Uuid $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    public static function fromString(string $value): self
+    {
+        return new self(Uuid::fromString($value));
+    }
+
+    public function toString(): string
+    {
+        return $this->uuid->toString();
+    }
+}
