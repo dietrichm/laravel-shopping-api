e917ba797aa1e71e6b7a19c8202eea4c7ff68f2d
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-02 17:51:38 +0100

    Route POST request for `/api/orders` to Orders CreateController

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('products', 'Products\IndexController');
+Route::post('orders', 'Orders\CreateController');
