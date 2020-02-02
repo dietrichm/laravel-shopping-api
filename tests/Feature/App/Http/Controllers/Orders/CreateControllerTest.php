c216505d949d1636cd2bc8474503ce1518248181
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-02 17:48:42 +0100

    Assert order create controller creates Order and returns ID as JSON
    We replace the Bus by a fake so we can make assertions about commands
    (Laravel jobs) to be dispatched. By capturing the OrderId we can assert
    the right ID to be returned.

---
+++ tests/Feature/App/Http/Controllers/Orders/CreateControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Orders;
+
+use Domain\Orders\CreateOrder;
+use Domain\Orders\OrderId;
+use Illuminate\Support\Facades\Bus;
+use Tests\TestCase;
+
+final class CreateControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itCreatesOrderAndReturnsOrderIdAsJson(): void
+    {
+        /** @var OrderId|null $orderId */
+        $orderId = null;
+
+        Bus::fake();
+
+        $response = $this->postJson('/api/orders');
+
+        Bus::assertDispatched(
+            CreateOrder::class,
+            function (CreateOrder $command) use (&$orderId) {
+                $orderId = $command->getOrderId();
+
+                return true;
+            }
+        );
+
+        $response->assertJsonPath('data.id', $orderId->toString());
+    }
+}
