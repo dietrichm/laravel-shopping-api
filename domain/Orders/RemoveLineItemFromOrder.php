5da8f04b3374a3c38295fb1ed8d862e818d13415
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-04 14:18:00 +0100

    Add command to remove LineItem from Order

---
+++ domain/Orders/RemoveLineItemFromOrder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Domain\Orders;
+
+final class RemoveLineItemFromOrder
+{
+    /**
+     * @var LineItemId
+     */
+    private $lineItemId;
+
+    /**
+     * @var OrderId
+     */
+    private $orderId;
+
+    public function __construct(
+        LineItemId $lineItemId,
+        OrderId $orderId
+    ) {
+        $this->lineItemId = $lineItemId;
+        $this->orderId = $orderId;
+    }
+
+    public function getLineItemId(): LineItemId
+    {
+        return $this->lineItemId;
+    }
+
+    public function getOrderId(): OrderId
+    {
+        return $this->orderId;
+    }
+}
