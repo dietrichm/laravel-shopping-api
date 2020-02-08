69affa42a8fdc16f6fd82b4569507dfebb59ff13
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-08 17:49:06 +0100

    Assert line items with total value is included in Order array

--- tests/Unit/App/Http/Resources/Orders/OrderResourceTest.php
+++ tests/Unit/App/Http/Resources/Orders/OrderResourceTest.php
@@ -3,11 +3,15 @@
 namespace Tests\Unit\App\Http\Resources\Orders;
 
 use App\Http\Resources\Orders\OrderResource;
+use App\Http\Resources\Products\ProductResource;
+use Domain\Orders\LineItem;
+use Domain\Orders\LineItemId;
 use Domain\Orders\Order;
 use Domain\Orders\OrderId;
 use Domain\Orders\OrderWasCreated;
+use Domain\Products\Product;
 use Illuminate\Http\Request;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 final class OrderResourceTest extends TestCase
 {
@@ -31,6 +35,61 @@ final class OrderResourceTest extends TestCase
                 'removedLineItems' => [],
             ],
             $resource->toArray(new Request())
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itIncludesLineItemsInArray(): void
+    {
+        $orderId = OrderId::generate();
+
+        /** @var Order $order */
+        $order = (new Order())
+            ->recordThat(new OrderWasCreated($orderId));
+
+        $lineItemOne = $this->givenThereIsALineItem([
+            'price' => 85.11,
+        ]);
+        $order->getLineItems()->add($lineItemOne);
+
+        $lineItemTwo = $this->givenThereIsALineItem([
+            'price' => 19.2,
+        ]);
+        $order->getLineItems()->add($lineItemTwo);
+
+        $resource = new OrderResource($order);
+        $request = new Request();
+
+        $this->assertEquals(
+            [
+                'id' => $orderId->toString(),
+                'totalPrice' => 104.31,
+                'lineItems' => [
+                    [
+                        'id' => $lineItemOne->getId()->toString(),
+                        'product' => (new ProductResource($lineItemOne->getProduct()))
+                            ->toArray($request),
+                    ],
+                    [
+                        'id' => $lineItemTwo->getId()->toString(),
+                        'product' => (new ProductResource($lineItemTwo->getProduct()))
+                            ->toArray($request),
+                    ],
+                ],
+                'removedLineItems' => [],
+            ],
+            $resource->toArray($request)
+        );
+    }
+
+    private function givenThereIsALineItem(
+        array $productProperties = []
+    ): LineItem {
+        return new LineItem(
+            LineItemId::generate(),
+            factory(Product::class)->make($productProperties)
         );
     }
 }
