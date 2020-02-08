b9b140cd90ad65f61a15856150f155d1109acc56
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-08 17:50:02 +0100

    Transform line items with product into array
    We're reusing the ProductResource here and create a new ProductResource
    instance for every LineItem's product.

--- app/Http/Resources/Orders/OrderResource.php
+++ app/Http/Resources/Orders/OrderResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources\Orders;
 
+use App\Http\Resources\Products\ProductResource;
+use Domain\Orders\LineItem;
+use Domain\Orders\LineItemCollection;
 use Domain\Orders\Order;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -19,8 +22,35 @@ final class OrderResource extends JsonResource
         return [
             'id' => $order->getId()->toString(),
             'totalPrice' => $order->getTotalPrice()->getValue(),
-            'lineItems' => [],
+            'lineItems' => $this->transformLineItems(
+                $order->getLineItems(),
+                $request
+            ),
             'removedLineItems' => [],
+        ];
+    }
+
+    private function transformLineItems(
+        LineItemCollection $lineItems,
+        Request $request
+    ): array {
+        return array_map(
+            function (LineItem $lineItem) use ($request) {
+                return $this->transformLineItem($lineItem, $request);
+            },
+            $lineItems->all()
+        );
+    }
+
+    private function transformLineItem(
+        LineItem $lineItem,
+        Request $request
+    ): array {
+        $productResource = new ProductResource($lineItem->getProduct());
+
+        return [
+            'id' => $lineItem->getId()->toString(),
+            'product' => $productResource->toArray($request),
         ];
     }
 }
