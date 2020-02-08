7a9465dc7c3621f790375d46cb70f6c4bc2b7a55
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-08 19:28:03 +0100

    Return bad request when OrderDoesNotExist exception is thrown

--- app/Http/Controllers/Orders/ShowController.php
+++ app/Http/Controllers/Orders/ShowController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Orders;
 
 use App\Http\Resources\Orders\OrderResource;
 use Domain\Orders\Order;
+use Domain\Orders\OrderDoesNotExist;
 use Domain\Orders\OrderId;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 final class ShowController extends JsonResponse
 {
@@ -21,7 +23,12 @@ final class ShowController extends JsonResponse
         )->validate();
 
         $orderId = OrderId::fromString($orderIdString);
-        $order = Order::id($orderId);
+
+        try {
+            $order = Order::id($orderId);
+        } catch (OrderDoesNotExist $exception) {
+            throw new BadRequestHttpException($exception->getMessage());
+        }
 
         return OrderResource::make($order)
             ->toResponse($request);
