ded2622a0ff46d9b739ec76f53db30fbb31395f4
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-09 18:52:44 +0100

    Add EmailAddress value object to represent valid email addresses

---
+++ app/ValueObjects/EmailAddress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\ValueObjects;
+
+use RuntimeException;
+
+final class EmailAddress
+{
+    /**
+     * @var string
+     */
+    private $emailAddress;
+
+    public function __construct(string $emailAddress)
+    {
+        if (filter_var($emailAddress, FILTER_VALIDATE_EMAIL) === false) {
+            return new RuntimeException('Invalid email address provided');
+        }
+
+        $this->emailAddress = $emailAddress;
+    }
+
+    public static function fromString(string $emailAddress): self
+    {
+        return new self($emailAddress);
+    }
+
+    public function toString(): string
+    {
+        return $this->emailAddress;
+    }
+}
