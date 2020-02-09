21cf59798e06fed83e2b2ed140df14e0e49e5728
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-02-09 18:55:41 +0100

    Add OrderWasCheckedOut domain event that will be persisted on Order

---
+++ domain/Orders/OrderWasCheckedOut.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Domain\Orders;
+
+use App\ValueObjects\EmailAddress;
+
+final class OrderWasCheckedOut
+{
+    /**
+     * @var OrderId
+     */
+    private $orderId;
+
+    /**
+     * @var EmailAddress
+     */
+    private $emailAddress;
+
+    public function __construct(
+        OrderId $orderId,
+        EmailAddress $emailAddress
+    ) {
+        $this->orderId = $orderId;
+        $this->emailAddress = $emailAddress;
+    }
+
+    public function getOrderId(): OrderId
+    {
+        return $this->orderId;
+    }
+
+    public function getEmailAddress(): EmailAddress
+    {
+        return $this->emailAddress;
+    }
+}
